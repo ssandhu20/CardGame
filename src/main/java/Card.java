85ea2ff5159488b96b23984538e688122dd3a7c5
sachinsandhu <sachin.sandhu@menloschool.org>
2025-11-17 13:26:00 -0800

    Test 2

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -3,5 +3,34 @@ public class Card {
     private String rank;
     private int value;
 
+    public Card(String suit, String rank, int value)
+    {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
 
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
