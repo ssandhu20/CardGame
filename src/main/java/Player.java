54b888668aacf201e844d6645a1b3c66397ab9e0
sachinsandhu <sachin.sandhu@menloschool.org>
2025-11-18 18:42:53 -0800

    Test 2

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+
 public class Player {
+    private String name;
+    private ArrayList<Card> hand;
+    private int points;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();
+        this.points = 0;
+    }
+
+    public Player(String name, ArrayList<Card> hand) {
+        this.name = name;
+        this.hand = hand;
+        this.points = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints(int points) {
+        this.points += points;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public String toString() {
+        return name + " has " + points + " points\n" +
+                name + "'s cards: " + hand;
+    }
 }
